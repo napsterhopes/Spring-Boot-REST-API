6c9d1a4f745de740890d97b16192cee9616b8b62
Harshit Gupta - h0g030n <Harshit.Gupta@walmart.com>
2024-05-17 15:40:27 +0530

    Path Variable in routes

--- src/main/java/SpringBoot_REST_Api/helloworld/HelloWorldController.java
+++ src/main/java/SpringBoot_REST_Api/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package SpringBoot_REST_Api.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +12,12 @@ public class HelloWorldController {
         return "Hello World";
     }
 
-    @GetMapping(path = "/hello-world-bean")
-    public HelloWorldBean helloWorldBean() {
-        return new HelloWorldBean("Hello World");
+    // Path Parameters
+    // /users/{id}/todos/{id}  => /users/2/todos/200
+    // /hello-world/path-variable/{name}
+    // /hello-world/path-variable/harshit
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
